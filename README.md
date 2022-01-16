bfb31e5210ed03411ecac3644050813dd9f788ee
joao2391 <j_paulo_15@hotmail.com>
2022-01-16 08:11:18 -0300

    feat: fix readme and add info to publish on hex

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
+[![Elixcep version](https://img.shields.io/hexpm/v/elixcep.svg)](https://hex.pm/packages/elixcep)
+[![Hex Docs](https://img.shields.io/badge/hex-docs-lightgreen.svg)](https://hexdocs.pm/elixcep/)
+[![Hex.pm](https://img.shields.io/hexpm/dt/elixcep.svg)](https://hex.pm/packages/)
+
 # Elixcep
 
-**TODO: Add description**
+Elixcep helps you to get a CEP or Address from brazilian's postal service
 
 ## Installation
 
@@ -15,7 +19,17 @@ def deps do
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/elixcep](https://hexdocs.pm/elixcep).
+## Features
+```elixir
+get_address_by_cep(cep)
+
+get_cep_by_address(address)
+```
+
+## Documentation
+
+Documentation can be found at [https://hexdocs.pm/elixcep](https://hexdocs.pm/elixcep).
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
 
--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Elixcep.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :elixcep,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12",
+      description: "Library to get cep and address from Correios",
+      source_url: "https://github.com/joao2391/elixcep",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: [source_ref: "#{@version}", extras: ["README.md"], main: "readme"]
     ]
   end
 
@@ -23,9 +29,21 @@ defmodule Elixcep.MixProject do
     [
       {:httpoison, "~> 1.8"},
       {:json, "~> 1.4"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp package do
+    [
+      maintainers: ["Joao Paulo de C. Lima"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/joao2391/elixcep",
+      }
+    ]
+  end
+
 end
